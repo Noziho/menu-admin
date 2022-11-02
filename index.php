ccaab16aa64ff2b4d4d1a853b67868e87cb5fc23
noahd <noah.decroix3@gmail.com>
2022-11-02 14:29:42 +0100

    menu without icon done

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="main_container">
+    <div id="navbar">
+        <div>
+            <h1>Basik</h1>
+        </div>
+
+        <div class="margin_div">
+            <p>Main</p>
+            <a id="main" href="https://google.fr" target="_blank">Dashboard</a>
+        </div>
+
+        <div class="margin_div">
+            <p>Applications</p>
+            <div>
+                <a href="https://google.fr" target="_blank">Calendar</a>
+            </div>
+
+            <div>
+                <a href="https://google.fr" target="_blank">Users</a>
+            </div>
+
+            <div>
+                <a href="https://google.fr" target="_blank">Messages</a>
+            </div>
+
+            <div>
+                <a href="https://google.fr" target="_blank">Email</a>
+            </div>
+        </div>
+
+        <div class="margin_div">
+            <p>Ui element</p>
+            <div>
+                <a href="https://google.fr" target="_blank">Components</a>
+            </div>
+            <div>
+                <a href="https://google.fr" target="_blank">Plugins</a>
+            </div>
+            <div>
+                <a href="https://google.fr" target="_blank">Form</a>
+            </div>
+            <div>
+                <a href="https://google.fr" target="_blank">Tables</a>
+            </div>
+            <div>
+                <a href="https://google.fr" target="_blank">Charts</a>
+            </div>
+        </div>
+
+        <div class="margin_div">
+            <p>Extra</p>
+            <div>
+                <a href="https://google.fr" target="_blank">Pages</a>
+            </div>
+
+            <div>
+                <a href="https://google.fr" target="_blank">Auth</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
